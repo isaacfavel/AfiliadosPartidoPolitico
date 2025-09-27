73bcff82e7782998870c8f97b8bcc4c6388ccedf
isaacfavel <pirrisfavela@gmail.com>
2025-09-26 19:11:40 -0600

    funciona

--- AfiliadosPartidoPolitico/Form1.cs
+++ AfiliadosPartidoPolitico/Form1.cs
@@ -39,6 +39,7 @@ namespace AfiliadosPartidoPolitico
                 txtEstado.Text ="Coahuila";
                 cargarDatos(archivo);
             }
+            
         }
 
         private void cargarDatos(string archivo)
