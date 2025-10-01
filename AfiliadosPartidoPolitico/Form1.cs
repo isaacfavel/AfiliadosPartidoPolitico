b7424096b9bdcecedfd9a73f3296c56ba2b7e777
isaacfavel <pirrisfavela@gmail.com>
2025-10-01 15:55:49 -0600

    funciona

--- AfiliadosPartidoPolitico/Form1.cs
+++ AfiliadosPartidoPolitico/Form1.cs
@@ -1,19 +1,11 @@
 ﻿using OfficeOpenXml;
 using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
 using System.IO;
-using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Xml;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
+
 
 namespace AfiliadosPartidoPolitico
 {
